6c91402533e98a1f675230b44b852ccf025ae1e7
Student <student@makerss-mbp-10.mynet>
2019-05-28 21:47:33 +0100

    Created erb file. Used .sample to make header and put in instance variable instead of code in erb

--- app.rb
+++ app.rb
@@ -13,8 +13,8 @@ get '/tan' do
   "My name"
 end
 
+
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-   </div>"
+  @random_name = ["Amigo", "Oscar", "Viking"].sample
+  erb :index
 end
